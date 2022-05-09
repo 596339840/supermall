b1ec3568fee5f3d694ae12251dd35b9b2750dda7
596339840 <596339840@qq.com>
2022-05-09 22:24:18 +0800

    第二次提交

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
 <template>
+  <div>
+      <h1>123</h1>
+  </div>
 </template>
 
 <script>
@@ -11,12 +14,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "assets/css/base.css";
 </style>
